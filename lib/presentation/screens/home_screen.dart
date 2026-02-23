f330e0d0ef64b21444bdbf42fa448ba10097c97c
Srinath <sri160697@gmail.com>
2026-02-23 18:06:19 +0530

    changes in home screen

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -8,7 +8,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Instagram Status UI")),
+      appBar: AppBar(title: const Text("Instagram Status UI Clone")),
       body: Column(
         children: [
           const StatusList(),
